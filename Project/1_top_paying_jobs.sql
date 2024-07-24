91564d187315657923df38c6c78bf8d3daa7cf8b
firate2 <158044351+firaterkn@users.noreply.github.com>
2024-07-24 19:29:42 +0300

    new changes

--- Project/1_top_paying_jobs.sql
+++ Project/1_top_paying_jobs.sql
@@ -11,13 +11,13 @@ Question: What are the top-paying data analyst jobs?
 
 SELECT	
 	job_id,
-    job_title_short,
+        job_title_short,
 	job_title,
 	job_location,
 	job_schedule_type,
 	salary_year_avg,
 	job_posted_date,
-    name AS company_name
+        name AS company_name
 FROM
     job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
